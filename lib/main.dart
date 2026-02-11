320356f3224df94a898497f14a93847c781d3249
karthikeya-tummala <karthikeyatummala@gmail.com>
2026-02-11 10:56:31 +0530

    Day 1 task

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'screens/dashboard_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Ground Control Station',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: const Color(0x0066CC)),
+      ),
+      home: const DashboardScreen(),
+    );
+  }
+}
